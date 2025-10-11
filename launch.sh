4d870e3ce24360cfe2eec7c7ab12e88f4438ebfa
khde <gaha.1@gmx.de>
2025-10-11 23:39:13 +0200

    Add docker compose file for homepage

--- launch.sh
+++ launch.sh
@@ -13,6 +13,7 @@ fi
 
 docker compose -f ./postgresql/docker-compose.yml "$ACTION" $FLAG
 docker compose -f ./keycloak/docker-compose.yml "$ACTION" $FLAG
+docker compose -f ./homepage/docker-compose.yml "$ACTION" $FLAG
 docker compose -f ./pgadmin/docker-compose.yml "$ACTION" $FLAG
 docker compose -f ./gitea/docker-compose.yml "$ACTION" $FLAG
 docker compose -f ./nextcloud/docker-compose.yml "$ACTION" $FLAG
