806b06a141efc007a7897a04afe604929cf8e450
khde <gaha.1@gmx.de>
2026-02-26 13:05:39 +0100

    Remove debug echos

--- docker/postgres/init.sh
+++ docker/postgres/init.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "DEBUG ================================================="
-echo "POSTGRES_USER     = [$POSTGRES_USER]"
-echo "POSTGRES_PASSWORD = [$POSTGRES_PASSWORD]"   # password will be visible in logs!
-echo ""
-echo "NPM_DB_USER       = [$NPM_DB_USER]"
-echo "NPM_DB_PASSWORD   = [$NPM_DB_PASSWORD]"
-echo "NPM_DB_NAME       = [$NPM_DB_NAME]"
-echo ""
-echo "NEXTCLOUD_DB_USER     = [$NEXTCLOUD_DB_USER]"
-echo "NEXTCLOUD_DB_PASSWORD = [$NEXTCLOUD_DB_PASSWORD]"
-echo "NEXTCLOUD_DB_NAME     = [$NEXTCLOUD_DB_NAME]"
-echo ""
-echo "GITEA_DB_USER     = [$GITEA_DB_USER]"
-echo "GITEA_DB_PASSWORD = [$GITEA_DB_PASSWORD]"
-echo "GITEA_DB_NAME     = [$GITEA_DB_NAME]"
-echo "======================================================="
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_USER" <<-EOSQL
 
     -- NPM
